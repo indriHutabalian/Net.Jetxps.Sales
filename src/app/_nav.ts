2be6b6c9fa0998e9119f9a18a0ebb89ac15ce2a9
adythiasjamsuri <adythia.sjamsuri@gmail.com>
2018-06-05 18:43:48 +0700

    initial commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,50 @@
+export const navItems = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Tasks'
+  },
+  {
+    name: 'Engagement Runsheet',
+    url: '',
+    icon: 'icon-calendar',
+    children: [
+      {
+        name: 'Create',
+        url: '/dashboard',
+        icon: ''
+      },
+      {
+        name: 'Proof of Engagement',
+        url: '/dashboard',
+        icon: ''
+      }
+    ]
+  },
+  {
+    name: 'Prospect Clients',
+    url: '/dashboard',
+    icon: 'icon-people'
+  },
+  {
+    title: true,
+    name: 'Report'
+  },
+  {
+    title: true,
+    name: 'History'
+  },
+  {
+    name: 'Engagement Runsheet',
+    url: '/dashboard',
+    icon: ''
+  },
+];
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+
+  apiAuthUrl: `http://jet-api-auth.azurewebsites.net`,
+  apiUrl: `http://jet-api-resource.azurewebsites.net`
+};
